0f4400f2803ba25b705cd7ea8cae1d232da7f8c7
ykB5330 <zhonghy2007@qq.com>
2026-07-10 11:03:15 +0800

    Add files via upload

---
+++ src/algorithms/clahe.py
@@ -0,0 +1,10 @@
+﻿from skimage.exposure import equalize_adapthist 
+from skimage.exposure import exposure
+import numpy as np
+def clahe_enhance(image, clip_limit=0.03):
+
+    img_norm = image.astype(float) / 255.0
+
+    enhanced = exposure.equalize_adapthist(img_norm, clip_limit=clip_limit, nbins=256, kernel_size=tile_size)
+    return (enhanced * 255).astype(np.uint8)
+
