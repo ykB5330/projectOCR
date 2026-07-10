5d6eef68ae011f9d82807ce29d77ec317bc5185d
ykB5330 <zhonghy2007@qq.com>
2026-07-10 09:57:32 +0800

    Refactor image preprocessing to use dynamic input path
    Refactor preprocess function to accept input path as an argument and update image processing steps accordingly. Save the preprocessed image to a specified output path.

--- src/image_utils.py
+++ src/image_utils.py
@@ -10,23 +10,15 @@ from algorithms.USM import usm_sharpen
 from algorithms.contrast_stretch import contrast_stretch
 from algorithms.gamma import gamma_correction
 
-INPUT_PATH = "test.jpg"
-OUTPUT_FINAL = "out_final.jpg"
-def preprocess(inage_path: str):
-  check_y, check_x = 60, 60
 
-  if __name__ == "__main__":
-      img_pil = Image.open(INPUT_PATH)
+def preprocess(inage_path: str):
+
+      img_pil = Image.open(image_path)
       rgb_data = np.array(img_pil)
+
 
       # 1. 自定义灰度转换
       gray_data = weighted_rgb2gray(rgb_data)
-      gray_pil = Image.fromarray(gray_data)
-
-      # 像素校验
-      r, g, b = rgb_data[check_y, check_x]
-      calc = 0.299 * r + 0.587 * g + 0.114 * b
-      pixel_out = gray_data[check_y, check_x]
 
 
       # 2. 中值滤波
@@ -50,7 +42,7 @@ def preprocess(inage_path: str):
       if abs(angle)>1:
           rotated_gray_pil=rotate_image(med_pil,angle)
       else:
-          rotated_gray_pil = gray_pil
+          rotated_gray_pil = med_pil
       rotated_gray_data=np.array(rotated_gray_pil)
       #5. USM锐化
       usm_data=usm_sharpen(rotated_gray_data, radius=1.0, amount=1.0,threshold=0)
@@ -63,5 +55,8 @@ def preprocess(inage_path: str):
 
       #输出最终结果
       return final_pil
+  
 
 
+preprocessed_img=preprocess(r"C:\Users\AW\Desktop\general_ocr_002.png")
+preprocessed_img.save(r"C:\Users\AW\Desktop\general_ocr_002_preprocessed.png")
